34860534314730d2a753c366f41377e3029cfcf9
jasrajsb <36668468+jasrajsb@users.noreply.github.com>
2021-03-04 18:47:11 +0530

    FloatingActionButton now opens a blank drawing page

--- drawing_app/lib/main.dart
+++ drawing_app/lib/main.dart
@@ -42,7 +42,10 @@ class listOfDrawingsState extends State{
         tooltip: "Add",
         child: Icon(Icons.add),
         onPressed: () {
-
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CanvasPage()),
+          );
         },
       ),
     );
@@ -95,4 +98,64 @@ class MyListState extends State<MyList> {
       },
     );
   }
+}
+
+
+class CanvasPage extends StatefulWidget {
+  @override
+   createState() => new CanvasState();
+}
+
+class CanvasState extends State {
+  List<Offset> _points = <Offset>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        child: new GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox object = context.findRenderObject();
+              Offset _localPosition =
+              object.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)..add(_localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: new CustomPaint(
+            painter: new Signature(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.clear),
+        onPressed: () => _points.clear(),
+      ),
+    );
+  }
+}
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
 }
